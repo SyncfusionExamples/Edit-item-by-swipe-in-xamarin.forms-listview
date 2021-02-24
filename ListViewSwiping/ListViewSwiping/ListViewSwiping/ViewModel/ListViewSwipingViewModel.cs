2633f6204296147f8bcbe3eb40d2bb99834150d5
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2021-02-24 09:56:00 +0530

    XAMARIN-39158 Sample updated to latest nuget

--- ListViewSwiping/ListViewSwiping/ListViewSwiping/ViewModel/ListViewSwipingViewModel.cs
+++ ListViewSwiping/ListViewSwiping/ListViewSwiping/ViewModel/ListViewSwipingViewModel.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SwipeEndedEventArgs = Syncfusion.ListView.XForms.SwipeEndedEventArgs;
+using SwipeStartedEventArgs = Syncfusion.ListView.XForms.SwipeStartedEventArgs;
 
 namespace Swiping
 {
